5cefb0a1fe0eb34621c00604e18edb35bbaf67f6
Ilshat300014 <ilshatkhabriev@yandex.ru>
2023-02-02 10:22:24 +0300

    task 4 works

--- task_4/javaScript.js
+++ task_4/javaScript.js
@@ -1,16 +1,28 @@
  const btnGetData = document.querySelector('button');
+ const errorNodeForWidth = document.querySelector('.error__message__width');
+ const errorNodeForHigh = document.querySelector('.error__message__high');
+ const resultNode = document.querySelector('.result');
  btnGetData.addEventListener('click', () => {
-   const inputWidth = document.querySelector('.input__width').value;
-   const inputHigh = document.querySelector('.input__high').value;
+   const inputWidth = document.querySelector('.form__input__width').value;
+   const inputHigh = document.querySelector('.form__high__high').value;
    const width = checkValue(inputWidth);
    const high = checkValue(inputHigh);
-   if (width && high){
-     fetch(`https:picsum.photos/${width}/${high}`)
-       .then((response) => { console.log(response.json()); })
+   if (!width) {
+     errorNodeForWidth.innerHTML = 'Oдно из чисел вне диапазона от 100 до 300';
+   } else if (!high) {
+        errorNodeForWidth.innerHTML = '';
+        errorNodeForHigh.innerHTML = 'Oдно из чисел вне диапазона от 100 до 300';
    } else {
-     console.log('Oдно из чисел вне диапазона от 100 до 300')
-   };
- });
+        errorNodeForWidth.innerHTML = '';
+        errorNodeForHigh.innerHTML = '';
+        fetch(`https://picsum.photos/${width}/${high}`)
+            .then((response) => {return response.blob()})
+            .then((blob) => {
+                const objectURL  = URL.createObjectURL(blob);
+                return objectURL })
+            .then((objectURL) => {displayResult(objectURL)})
+        }
+   });
  function checkValue(value) {
    const convertNumber = Number(value);
    if (convertNumber < 301 &&
@@ -21,7 +33,17 @@
    }
  }
 
-//fetch('https://picsum.photos/200/300')
-//      .then((response) => { return response.json(); })
-//      .then((data) => { console.log(data); })
-//      .catch((error) => { console.log(error.message); })
+function displayResult(url) {
+  let card = `
+      <div class="card">
+        <img
+          src="${url}"
+          class="card-image"
+        />
+      </div>
+    `;
+  resultNode.innerHTML = card;
+}
+
+
+
